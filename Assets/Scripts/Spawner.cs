113401141178140abaff303fa23d8cbae7208247
EsamSmesm <EsamSmesm@users.noreply.github.com>
2019-04-03 21:15:55 +0200

    Initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    //groups
+    public GameObject[] groups;
+
+    public void SpawnNext()
+    {
+        // random index
+        int i = Random.Range(0, groups.Length);
+
+        // spawn group at current location
+        Instantiate(groups[i],
+            transform.position,
+            Quaternion.identity);
+    }
+
+	// Use this for initialization
+	void Start () {
+        SpawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
